b3d6edb5878be61f6740d3432750173534a25b8b
cnelson20 <cjnelson2004@gmail.com>
2021-12-08 09:27:26 -0500

    code

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,131 @@
-from flask import Flask, render_template
-import urllib3, json
+from flask import Flask, render_template, request, session
+import urllib3, json, os, sqlite3
 
 app = Flask(__name__)
-http = urllib3.poolManager()
+app.secret_key = os.urandom(32);
+http = urllib3.PoolManager()
+
+MAIN_DB = "users.db"
+
+# database
+db = sqlite3.connect(MAIN_DB)
+c = db.cursor()
+
+# table creation
+c.execute("""
+CREATE TABLE IF NOT EXISTS USERS (
+    ROWID       INTEGER PRIMARY KEY,
+    USERNAME    TEXT    NOT NULL,
+    HASH        TEXT    NOT NULL,
+    PFP         TEXT   
+);""")
+
+db.commit()
+db.close()
 
 @app.route("/")
 def index():
     return "";
+
+# Signup function
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    """
+        If method = GET, render page to new username & password
+        If method = POST, attempts to sign up user, if successful renders login data
+    """
+    # Obtaining query from html form
+    if request.method == "POST":
+        # Checking if required values in query exist using key values
+        if 'username' in request.form and 'password' in request.form:
+            db = sqlite3.connect(MAIN_DB)
+            c = db.cursor()
+            # Obtaining data from database
+            c.execute("""SELECT USERNAME FROM USERS WHERE USERNAME = ?;""",
+                      (request.form['username'],))
+            exists = c.fetchone()
+            # Checking to see if the username that the person signing up gave has not been made
+            if (exists == None):
+                username = (request.form['username']).encode('utf-8')
+                # Check to see if user follows formatting
+                if isAlphaNum(username.decode('utf-8')) == None:
+                    db.close()
+                    return render_template("login.html", user=session.get('username'), action="/signup", name="Sign Up", error="Username can only contain alphanumeric characters and underscores.")
+                # Check to see if username is of proper length
+                if len(username) < 5 or len(username) > 15:
+                    db.close()
+                    return render_template("login.html", user=session.get('username'), action="/signup", name="Sign Up", error="Usernames must be between 5 and 15 characters long")
+                password = request.form['password']
+                # Checking for illegal characters in password
+                if ' ' in list(password) or '\\' in list(password):
+                    db.close()
+                    return render_template("login.html",action="/signup", name="Sign Up", error="Passwords cannot contain spaces or backslashes.")
+                password = str(password)
+                # Checking to see if password follows proper length
+                if len(password) > 7 and len(password) <= 50:
+                    r = http.request('GET',"http://dog.ceo/api/breeds/image/random");
+                    pfpurl = "";
+                    if r.status == 200:
+                        pfp = json.loads(r.data).get('message');
+                    c.execute("""INSERT INTO USERS (USERNAME,HASH,PFP) VALUES (?,?)""",(request.form['username'], password,pfpurl,));
+                    db.commit()
+                    c.execute(
+                        """SELECT USERNAME FROM USERS WHERE USERNAME = ?;""", (request.form['username'],))
+                    exists = c.fetchone()
+                    db.close()
+                    if (exists != None):
+                        return render_template("login.html", action="/login", name="Login", success="Signed up successfully!")
+                    else:
+                        return render_template("login.html", action="/signup", name="Sign Up", error="Some error occurred. Please try signing up again.")
+                else:
+                    db.close()
+                    return render_template("login.html", action="/signup", name="Sign Up", error="Password must be between 8 and 50 characters long")
+            else:
+                db.close()
+                return render_template("login.html", action="/signup", name="Sign Up", error="Username already exists")
+        else:
+            return render_template("login.html", action="/signup", name="Sign Up", error="Some error occurred. Please try signing up again.")
+    else:
+        return render_template("login.html", action="/signup", name="Sign Up")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """
+        If method = GET, render page to enter login info
+        If method = POST, attempts to login user with posted data
+    """
+    if request.method == "POST":
+        if 'username' in session:
+            return render_template("index.html", user=session.get('username'), message="Already logged in!")
+        if 'username' in request.form and 'password' in request.form:
+            db = sqlite3.connect(MAIN_DB)
+            c = db.cursor()
+            c.execute("""SELECT HASH FROM USERS WHERE USERNAME = ?;""",
+                      (request.form['username'],))
+            hashed = c.fetchone()  # [0]
+            db.close()
+            if (hashed == None):
+                return render_template("login.html", name="Login", action="/login", error="User does not exist.")
+            else:
+                if hashed[0] == request.form['password']:
+                    session['username'] = request.form['username']
+                    return render_template("index.html", message="Logged in!")
+                else:
+                    return render_template("login.html", name="Login", action="/login", error="Password is incorrect")
+        else:
+            return render_template("login.html", name="Login", action="/login", error="An error occurred. Please try logging in again.")
+    else:
+        return render_template("login.html", action="/login", name="Login")
+
+# Logout function
+@app.route("/logout")
+def logout():
+    """ 
+        Logouts user 
+    """
+    session.pop('username', default=None)
+    return redirect("/")
+
 
 
 if __name__ == "__main__":
