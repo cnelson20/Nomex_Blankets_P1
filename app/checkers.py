4da5d50b0feffaf6110669510648ba1551e7f902
cnelson20 <cjnelson2004@gmail.com>
2021-12-16 15:16:38 +0000

    checkers

--- app/checkers.py
+++ app/checkers.py
@@ -27,17 +27,15 @@ def generate_board():
 
     return board;
 
-time_dict = dict();
-
 def start_game():
     if (not 'username' in session) or user in games_dict:
-        return false;
+        return False;
     else:
         session['game'] = dict();
         session['game']['board'] = generate_board();
-        session['game']['turn'] = 1;
+        session['game']['turn'] = 0;
         # generate emojis
-        return true;
+        return True;
 
 def remove_game():
     ''' 
@@ -45,35 +43,80 @@ def remove_game():
     '''
     if 'game' in session:
         del session['game'];
-        return true
-    return false
+        return True
+    return False
 
 def game_exists():
     return 'game' in session;
+
+def turns(doublehop):
+    if (not doublehop):
+        session['game']['turn'] = invert10(session['game']['turn']);
+    return True
+
+def clear_unused():
+    for y in range(8):
+        for x in range(8):
+            if (x + y) % 2 == 0:
+                session['game']['board'][y][x] = 0;
 
 def move(oldx,oldy,movex,movey):
     '''
        move piece 
     '''
     if 'game' in session:
-        print(session['game']);
         game_turn = session['game']['turn'];
         board = session['game']['board'];
         doublehop = False;
-        
+
+        if (movey < 0 or movey > 7 or movex < 0 or movex > 7 or oldy < 0 or oldy > 7 or oldx < 0 or oldy > 7):
+            print("OOB")
+            return False
+        if (movey + movex) % 2 == 0:
+            print("Wrong squares")
+            return False
         if (board[oldy][oldx] != 0 and board[oldy][oldx] % 2 == game_turn):
             ''' correct player continue checking things '''
             if (board[movey][movex] != 0 and board[movey][movex] % 2 == game_turn):
-                return false # if spot to move to is own team fail.
+                print("Move onto teammate")
+                return False # if spot to move to is own team fail.
         else:
-            return false
-        
-        if (not doublehop):
-            user_game['turn'] = (game_turn % 2) + 1;
-        return true
-    else:
-        return false  
+            print("Not your piece!")
+            return False        
 
-    
+        if (game_turn == 0 or board[oldy][oldx] >= 3):
+            if ((movex == oldx - 1 and movey == oldy - 1) or (movex == oldx + 1 and movey == oldy - 1)) and board[movey][movex] == 0:
+                board[movey][movex] = board[oldy][oldx];
+                if (movey == 0 and board[movey][movex] <= 2):
+                    board[movey][movex] += 2
+                board[oldy][oldx] = 0;
+                return turns(doublehop);
+        if (game_turn == 1 or board[oldy][oldx] >= 3):
+            if ((movex == oldx - 1 and movey == oldy + 1) or (movex == oldx + 1 and movey == oldy + 1)) and board[movey][movex] == 0:
+                board[movey][movex] = board[oldy][oldx];
+                if (movey == 0 and board[movey][movex] <= 2):
+                    board[movey][movex] += 2
+                board[oldy][oldx] = 0;
+                return turns(doublehop);
+    else:
+        print("game not in session")
+        # print(session)
+        return False  
+
+session = dict();
 if __name__ == "__main__":
-    print(*generate_board(),sep='\n');
+    session['user'] = 'username';
+    session['game'] = dict();
+    session['game']['board'] = generate_board()
+    session['game']['turn'] = 0;
+    while True:
+        print(*session['game']['board'],sep="\n");
+        print("Turn: " + str(1 + invert10(session['game']['turn'])))
+        first = True
+        inp = [0,0,0,0]
+        while first or not move(int(inp[0]),int(inp[1]),int(inp[2]),int(inp[3])):
+            first = False
+            inp = input(": ");
+            inp = inp.split(" ");
+        # clear_unused();
+    
